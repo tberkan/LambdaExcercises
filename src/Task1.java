9d24b3b3e54a0dccf47446455b367d001d5beb71
Berkan Telovski <btelovski@endava.com>
2018-11-08 09:32:42 +0100

    Solved Task 1 without Stream

--- src/Task1.java
+++ src/Task1.java
@@ -1,28 +1,28 @@
-import static java.lang.System.*;
-
 import domain.Task;
 import domain.TaskType;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+
+import static java.lang.System.out;
 
 public class Task1 {
-
     public static void main(String[] args) {
         List<Task> tasks = Task.getTasks();
-        List<String> titles = taskTitles(tasks);
+        Predicate<Task> readingTasks = t -> t.getType() == TaskType.READING;
+        List<String> titles = taskTitles(tasks, readingTasks);
         titles.forEach(out::println);
     }
 
-    private static List<String> taskTitles(List<Task> tasks) {
-        Predicate<Task> readingTasks = t -> t.getType() == TaskType.READING;
-        return tasks
-                .stream()
-                .filter(readingTasks)
-                .map(Task::getTitle)
-                .collect(Collectors.toList());
-
+    private static List<String> taskTitles(List<Task> tasks, Predicate<Task> predicate) {
+        List<String> titles = new ArrayList<>();
+        Function<Task, String> function = Task::getTitle;
+        for (Task task : tasks) {
+            if (predicate.test(task)) {
+                titles.add(function.apply(task));
+            }
+        }
+        return titles;
     }
-
 }
---
+++ src/Task1Stream.java
@@ -0,0 +1,27 @@
+import static java.lang.System.*;
+
+import domain.Task;
+import domain.TaskType;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Task1Stream {
+
+    public static void main(String[] args) {
+        List<Task> tasks = Task.getTasks();
+        List<String> titles = taskTitles(tasks);
+        titles.forEach(out::println);
+    }
+
+    private static List<String> taskTitles(List<Task> tasks) {
+        Predicate<Task> readingTasks = t -> t.getType() == TaskType.READING;
+        return tasks
+                .stream()
+                .filter(readingTasks)
+                .map(Task::getTitle)
+                .collect(Collectors.toList());
+    }
+
+}
